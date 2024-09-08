91590e15471038148c4b88bc888f7c21f40ee597
Micah Riggan <micahriggan@gmail.com>
2024-09-08 09:25:31 -0400

    updating github and knowhow embedding

--- src/services/GitHub.ts
+++ src/services/GitHub.ts
@@ -1,4 +1,5 @@
 import { Octokit } from "@octokit/rest";
+import axios from "axios";
 
 export class GitHubService {
   octokit: Octokit;
@@ -9,30 +10,55 @@ export class GitHubService {
     });
   }
 
+  private isLfsFile(content: string): boolean {
+    return content.startsWith("version https://git-lfs.github.com/spec/v1");
+  }
+
+  private getLfsPointerContent(content: string) {
+    const oid = content.split("\n").find((line) => line.startsWith("oid"));
+    const second = oid?.split(" ")[1];
+    const sha = second.split(":")[1];
+    return sha;
+  }
+
+  private async getLfsContent(downloadUrl: string): Promise<string> {
+    const response = await axios.get(downloadUrl);
+    return JSON.stringify(response.data);
+  }
+
+  private async getFileContent(
+    owner: string,
+    repo: string,
+    path: string
+  ): Promise<string> {
+    const { data } = await this.octokit.repos.getContent({
+      owner,
+      repo,
+      path,
+    });
+    if (!("content" in data)) {
+      throw new Error("File content not found in GitHub API response");
+    }
+    const content = Buffer.from(data.content, "base64").toString("utf-8");
+    console.log(data);
+    return this.isLfsFile(content)
+      ? await this.getLfsContent(data.download_url)
+      : content;
+  }
+
   async downloadFile(
     orgProject: string,
     fileName: string,
     destinationPath: string
   ): Promise<void> {
-    const [org, project] = orgProject.split("/");
-    const response = await this.octokit.repos.getContent({
-      owner: org,
-      repo: project,
-      path: fileName,
-    });
-    const fileData = response.data;
-    if ("content" in fileData && fileData.content) {
-      const content = Buffer.from(fileData.content, "base64").toString("utf-8");
-      const fs = require("fs");
-      fs.writeFileSync(destinationPath, content);
-      console.log(
-        `File ${fileName} downloaded from GitHub: ${orgProject} to ${destinationPath}`
-      );
-    } else {
-      throw new Error(
-        `File ${fileName} not found in GitHub repo: ${orgProject}`
-      );
-    }
+    const [owner, repo] = orgProject.split("/");
+    const content = await this.getFileContent(owner, repo, fileName);
+    console.log(`File content: ${content}`);
+    const fs = require("fs");
+    fs.writeFileSync(destinationPath, content);
+    console.log(
+      `File ${fileName} downloaded from GitHub: ${orgProject} to ${destinationPath}`
+    );
   }
 }
 
